4d51124d2dd85628ff1b49035eb1d0918f565d16
AUGVV <68101056+AUGVV@users.noreply.github.com>
2021-08-18 20:29:06 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Size = 10;
+            int[,] integer = new int[Size, Size];
+            Random rand = new Random();
+            Console.WriteLine("Massive");
+            while (true)
+            {
+                    for (int i = 0;i<9;i++)
+                    {
+                        MassiveOp(integer, Size, i, i+1);
+                        Thread.Sleep(300);
+                        Console.Clear();
+                    }
+            }
+
+            static  void MassiveOp(int[,] integer, int Size, int Number1 = 0, int Number2 = 1)
+            {
+                for (int i = 0; i < Size; i++)
+                {
+                    int sum = 0;
+                    Console.WriteLine("");
+                    for (int j = 0; j < Size; j++)
+                    {
+                        if (j == i) //Диагонваль
+                        {
+                            integer[i, j] = Number1;
+                            sum = sum + Number1;
+                        }
+                        else if (j == Size - 1 - i) //дополнительная диагональ
+                        {
+                            integer[i, j] = Number1;
+                            sum = sum + Number1;
+                        }
+                        else if (i == i - j) //линия столбец
+                        {
+                            integer[i, j] = Number1;
+                            sum = sum + Number1;
+                        }
+                        else if (i == Size - 1) //линия строка низ
+                        {
+                            integer[i, j] = Number1;
+                            sum = sum + Number1;
+                        }
+                        else if (i == 0) //линия строка верх
+                        {
+                            integer[i, j] = Number1;
+                            sum = sum + Number1;
+                        }
+                        else if (j == Size - 1) //линия последняя колонка
+                        {
+                            integer[i, j] = Number1;
+                            sum = sum + Number1;
+                        }
+                        else
+                        {
+                            integer[i, j] = Number2;
+                            sum = sum + Number2;
+                        }
+                        Console.Write(integer[i, j]);
+                    }
+                    Console.Write($" = {sum}");
+                }
+            }
+
+        }
+    }
+}
